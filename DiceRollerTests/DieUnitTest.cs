69d4e2f815afd050ff362f1d5ad616728af4f5ff
Moxit <shah.moxit999@gmail.com>
2022-08-08 17:08:41 -0400

    Added Mstest for die
    Added test to
    1. check whether the die class exists or not.
    2. check default value
    3.check sides are six and are of range 1-6.

---
+++ DiceRollerTests/DieUnitTest.cs
@@ -0,0 +1,44 @@
+﻿namespace DiceRollerTests;
+
+[TestClass]
+public class DieUnitTest
+{
+    Die d = new Die();
+
+
+    [TestMethod]
+    public void DieClassNotNull()
+    {
+
+        d.Should().NotBeNull();
+
+    }
+
+    [TestMethod]
+    public void DieHasAllDefaultValue()
+    {
+
+        d.Name.Should().Be("d6");
+
+        d.NumberSides.Should().Be(6);
+
+        d.CurrentSide.Should().BeInRange(1, 6);
+
+    }
+
+    [TestMethod]
+    public void DefaultRollSetsSideCorrectly()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+
+            d.Roll();
+
+            d.CurrentSide.Should().BeInRange(1, 6);
+
+
+        }
+    }
+
+}
+
